b16ec560fd0c8dc93cdfc592907332b6ba9ff386
Harold Kim <hyunoh4u@hotmail.com>
2014-03-25 22:55:40 +0900

    k

--- NamooClubThreeWeb/src/com/namoo/ns1/web/controller/aaaa.java
+++ NamooClubThreeWeb/src/com/namoo/ns1/web/controller/aaaa.java
@@ -3,8 +3,5 @@ package com.namoo.ns1.web.controller;
 public class aaaa {
 	public static void main(String str) {
 		//
-		System.out.println("I am a boy.");
-		
-		System.out.println("You are a girl");
 	}
 }
