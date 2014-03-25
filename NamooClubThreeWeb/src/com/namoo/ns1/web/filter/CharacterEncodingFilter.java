00041cc69e65fbae565ce235bbba0ce8343fa16d
Harold Kim <hyunoh4u@hotmail.com>
2014-03-26 01:46:32 +0900

    a

--- NamooClubThreeWeb/src/com/namoo/ns1/web/filter/CharacterEncodingFilter.java
+++ NamooClubThreeWeb/src/com/namoo/ns1/web/filter/CharacterEncodingFilter.java
@@ -6,17 +6,11 @@ import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.annotation.WebFilter;
-import javax.servlet.annotation.WebInitParam;
 
-@WebFilter(
-	urlPatterns = {"/*"}, 
-	initParams = {
-		@WebInitParam(name = "encoding", value = CharacterEncodingFilter.ENCODING )
-	}	
-)
+@WebFilter(urlPatterns = {"/*"})
 public class CharacterEncodingFilter extends SimpleFilterAdapter {
 
-	protected static final String ENCODING = "UTF-8";
+	private static final String ENCODING = "UTF-8";
 
 	@Override
 	public boolean doFilter(ServletRequest req, ServletResponse resp) throws IOException, ServletException {
